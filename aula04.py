774ce701d462f9867b1d687983809d2f2e821d9d
Marcelo8819 <silva.marcelohg@gmail.com>
2022-10-11 08:58:09 -0300

    Atualização de conteudo.

---
+++ aula04.py
@@ -0,0 +1,17 @@
+'''Primeiros comandos em Python'''
+
+# print('Olá, Mundo!')
+#
+# print('7+4')
+#
+# print(7+4)
+#
+# nome = 'Marcelo'
+# idade = 34
+# peso = 72.00
+# print(nome,idade,peso)
+
+nome = input('Qual o seu nome? ')
+idade = input('Qual a sua idade? ')
+peso = input('Qual o seu peso? ')
+print('Olá',nome,'sua idade é',idade, ' e seu peso é',peso)
