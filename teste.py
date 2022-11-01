19fe9fb6af854bf00809f79917311fe3b2570ca2
Marcelo8819 <silva.marcelohg@gmail.com>
2022-10-31 23:28:47 -0300

    Entrada de dados

--- teste.py
+++ teste.py
@@ -1,15 +1,7 @@
 # game loop
-while 1:
-    enemy_1 = input()  # name of enemy 1
-    dist_1 = int(input())  # distance to enemy 1
-    enemy_2 = input()  # name of enemy 2
-    dist_2 = int(input())  # distance to enemy 2
+def find_max(nums):
+    max_num = float("-inf")
+    for num in nums:
+        if num > max_num:
 
-    if dist_1 < dist_2:
-        print(enemy_1)
-    else:
-        print(enemy_2)
-
-    # Write an action using print
-
-    # Enter the code hereeasy
+    return  max_num
