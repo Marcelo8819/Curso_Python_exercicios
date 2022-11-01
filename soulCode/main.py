dc7a32606d425876ea978b284d173940bd00b53c
Marcelo8819 <silva.marcelohg@gmail.com>
2022-10-31 21:55:18 -0300

    saida de dados

---
+++ soulCode/main.py
@@ -0,0 +1,25 @@
+'''
+Aqui temos as variáveis com nomes de animais, quantidades e peso dos animais
+'''
+
+animal1 =  'Leao'
+animal2 = 'Tartaruga'
+animal3 = 'Cachorro'
+
+quantidade_de_animais = 3
+
+peso_animal1 = 60.8
+peso_animal2 = 32.4
+peso_animal3 = 25.6
+
+# Situação de vacinação
+
+situacao_animal1 = True
+situacao_animal2 = False
+situacao_animal3 = False
+
+print(animal1,animal2,animal3)
+print("O nome do animal e {}, seu peso é de {}kg".format(animal1,peso_animal1))
+
+
+
