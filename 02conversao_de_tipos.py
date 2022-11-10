24b6fc8ba6274fee20be0d789c2f3c421ce3663a
Marcelo8819 <silva.marcelohg@gmail.com>
2022-11-10 11:21:43 -0300

    operadores aritméticos

---
+++ 02conversao_de_tipos.py
@@ -0,0 +1,15 @@
+print(int(1.9)) # conversão de float para int 
+print(int("10")) # conversão de string para int
+print(float("10.10")) # conversão de string para float
+print(float(100)) #conversão de int para float
+
+print(type(str(10.10)))
+
+valor = 10 
+valor_str = str(valor)
+print(type(valor)) 
+print(type(valor_str))
+
+print(100/2) # está divisão retorna um número real 
+print(100//2) # está adivisão retorma um número inteiro 
+
---
+++ 04operadores_aritmeticos.py
@@ -0,0 +1,20 @@
+produto_1 = 20
+produto_2 = 10
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 / produto_2)   # divisão real 
+print(produto_1 // produto_2)  # divisão inteira 
+print(produto_1 * produto_2)
+print(produto_1 % produto_2)   # resto da divisão
+print(produto_1 ** produto_2)
+
+
+x = 10 + 5 * 4 
+print(x)
+
+x = (10+5)*4 
+print(x)
+
+y = 10 / 2 + 25 * 2 - 2 ** 2  # seguindo a ordem de precedência a operação começará pela exponenciação, em seguida pela multiplicação
+print(y)
