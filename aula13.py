73294898452239f894ad7611baa7b4608b176048
Marcelo8819 <silva.marcelohg@gmail.com>
2022-11-14 10:46:23 -0300

    usei laço for e biblioteca from time import sleep

---
+++ aula13.py
@@ -0,0 +1,50 @@
+# for i in range(0,10):
+#     print('passo')
+#     print('pula')
+# print('passo')
+# print('pego')
+
+# print('Oi')
+# print('Oi')
+# print('Oi')
+# print('Oi')
+# print('Oi')
+# print('Oi')
+
+# for c in range(0, 6):
+#     print('Oi')
+# print('Fim')
+
+# for c in range(7, 0, -1):
+#     print(c)
+# print('Fim')
+
+# for c in range(10, 0, -2):
+#     print(c)
+# print('Fim')
+
+# for c in range(1,  11):
+#     print(c)
+# print('Fim')
+
+# for c in range(0, 10, 2):
+#     print(c)
+# print('Fim')
+
+# n = int(input('Digite um número: '))
+# for c in range(1, n+1):
+#     print(c)
+# print('Fim')
+
+# i = int(input('Início: '))
+# f = int(input('Fim: '))
+# p = int(input('Passo: '))
+# for c in range(i, f+1, p):
+#     print(c)
+# print('Fim')
+
+for c in  range(0, 4):
+    n = int(input('Digite um valor: '))
+print('Fim')
+
+
