90635f639ae21d0454bf83ea8e2691b271a43c56
Marcelo8819 <silva.marcelohg@gmail.com>
2022-11-21 16:40:40 -0300

    manipulação de strings

---
+++ 22string_2.py
@@ -0,0 +1,7 @@
+nome = 'Marcelo'
+idade = 34
+profissao = "Programador"
+linguagem = 'Python'
+saldo = 50.000
+
+print(f'Meu nome é {nome}, tenho {34} anos de idade, minha profissão é {profissao} e a linguagem que  atualmente uso é a {linguagem}. O saldo na minha conta bancária é de {saldo:5.3f}')
---
+++ 23string_3.py
@@ -0,0 +1,8 @@
+nome = 'Marcelo Henrique Guilherme da Silva'
+
+print(nome[0])
+print(nome[:7])
+print(nome[8:16])
+print(nome[8:16:2])
+print(nome[:])
+print(nome[::-1])
